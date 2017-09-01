0a3a5e73e19228e222ddffcd4cde53545ec109bc
illuggah <illuggah@mail.ru>
2017-09-01 17:06:50 +0700

    Add files via upload

---
+++ list.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>JSON TEST form</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body style="background-color: #d5deed">
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-top">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="#" class="navbar-brand">JSON TEST form</a>
+			</div>
+			<div class="navbar-collapse navbar-top collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="admin.php">ЗАГРУЗИТЬ ТЕСТ</a></li>
+					<li><a href="list.php">СПИСОК ТЕСТОВ</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container" style="text-align: center;">
+		<h2>Список тестов</h2>
+			<?php
+				$get_catalog = scandir('uploaded_tests/');
+				foreach ($get_catalog as $key => $value) {
+					if ($value !== '.' && $value !== '..') {
+						echo '<a href="test.php?testfile=' . $value . '">'. json_decode(file_get_contents('uploaded_tests/' . $value), true)['testname'] .'</a><br>' . PHP_EOL;
+					}
+				}
+			?>	
+	</div>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
